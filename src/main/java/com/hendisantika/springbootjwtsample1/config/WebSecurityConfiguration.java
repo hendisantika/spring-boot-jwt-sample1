bba84dfdf921177c67986c0c14037fd4ebcf4f2a
hendisantika <hendisantika@yahoo.co.id>
2023-01-11 06:11:50 +0700

    #23 - Add authenticationManagerBean in WebSecurityConfiguration :green_book: :pencil2: :whale:

--- src/main/java/com/hendisantika/springbootjwtsample1/config/WebSecurityConfiguration.java
+++ src/main/java/com/hendisantika/springbootjwtsample1/config/WebSecurityConfiguration.java
@@ -1,7 +1,9 @@
 package com.hendisantika.springbootjwtsample1.config;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.core.userdetails.UserDetailsService;
@@ -27,4 +29,10 @@ public class WebSecurityConfiguration extends WebSecurityConfigurerAdapter {
     private final JwtRequestFilter jwtRequestFilter;
 
     private final UserDetailsService jwtService;
+
+    @Bean
+    @Override
+    public AuthenticationManager authenticationManagerBean() throws Exception {
+        return super.authenticationManagerBean();
+    }
 }

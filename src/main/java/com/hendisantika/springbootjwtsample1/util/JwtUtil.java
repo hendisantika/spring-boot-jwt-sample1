749c90924da16ee71aa8c4527d30637a8a35a595
hendisantika <hendisantika@yahoo.co.id>
2023-01-11 06:06:37 +0700

    #11 - Add getUsernameFromToken in JwtUtil :green_book: :pencil2: :whale:

--- src/main/java/com/hendisantika/springbootjwtsample1/util/JwtUtil.java
+++ src/main/java/com/hendisantika/springbootjwtsample1/util/JwtUtil.java
@@ -18,4 +18,8 @@ public class JwtUtil {
     private static final String SECRET_KEY = "learn_programming_yourself";
 
     private static final int TOKEN_VALIDITY = 3600 * 5;
+
+    public String getUsernameFromToken(String token) {
+        return getClaimFromToken(token, Claims::getSubject);
+    }
 }

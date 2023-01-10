572ade1666a38916c5f01db5774af3c4c2be33cb
hendisantika <hendisantika@yahoo.co.id>
2023-01-11 06:11:34 +0700

    #22 - Add WebSecurityConfiguration :green_book: :pencil2: :whale:

---
+++ src/main/java/com/hendisantika/springbootjwtsample1/config/WebSecurityConfiguration.java
@@ -0,0 +1,30 @@
+package com.hendisantika.springbootjwtsample1.config;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.UserDetailsService;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-boot-jwt-sample1
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 1/11/23
+ * Time: 06:11
+ * To change this template use File | Settings | File Templates.
+ */
+@Configuration
+@EnableWebSecurity
+@EnableGlobalMethodSecurity(prePostEnabled = true)
+@RequiredArgsConstructor
+public class WebSecurityConfiguration extends WebSecurityConfigurerAdapter {
+
+    private final JwtAuthenticationEntryPoint jwtAuthenticationEntryPoint;
+
+    private final JwtRequestFilter jwtRequestFilter;
+
+    private final UserDetailsService jwtService;
+}

a65fa9800527ef907a206ecab8af2cbcf859f358
hendisantika <hendisantika@yahoo.co.id>
2023-01-11 05:56:35 +0700

    #1 - Initial Commit :green_book: :pencil2: :whale:

---
+++ src/main/java/com/hendisantika/springbootjwtsample1/SpringBootJwtSample1Application.java
@@ -0,0 +1,13 @@
+package com.hendisantika.springbootjwtsample1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootJwtSample1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootJwtSample1Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/hendisantika/springbootjwtsample1/SpringBootJwtSample1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hendisantika.springbootjwtsample1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootJwtSample1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

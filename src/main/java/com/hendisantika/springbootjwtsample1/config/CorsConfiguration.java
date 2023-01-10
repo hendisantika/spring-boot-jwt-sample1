a7001bd933e5578746cd18138594e2367f15c32d
hendisantika <hendisantika@yahoo.co.id>
2023-01-11 06:09:06 +0700

    #18 - Add CorsConfiguration :green_book: :pencil2: :whale:

---
+++ src/main/java/com/hendisantika/springbootjwtsample1/config/CorsConfiguration.java
@@ -0,0 +1,38 @@
+package com.hendisantika.springbootjwtsample1.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-boot-jwt-sample1
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 1/11/23
+ * Time: 06:08
+ * To change this template use File | Settings | File Templates.
+ */
+@Configuration
+public class CorsConfiguration {
+    private static final String GET = "GET";
+    private static final String POST = "POST";
+    private static final String PUT = "PUT";
+    private static final String DELETE = "DELETE";
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedMethods(GET, POST, PUT, DELETE)
+                        .allowedHeaders("*")
+                        .allowedOriginPatterns("*")
+                        .allowCredentials(true);
+            }
+        };
+    }
+}

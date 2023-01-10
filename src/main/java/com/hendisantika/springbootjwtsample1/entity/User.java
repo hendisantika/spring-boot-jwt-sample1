67b1a2bbb447561e091a657aa73d888716deda10
hendisantika <hendisantika@yahoo.co.id>
2023-01-11 06:04:01 +0700

    #7 - Add User Entity :green_book: :pencil2: :whale:

---
+++ src/main/java/com/hendisantika/springbootjwtsample1/entity/User.java
@@ -0,0 +1,42 @@
+package com.hendisantika.springbootjwtsample1.entity;
+
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.JoinTable;
+import jakarta.persistence.ManyToMany;
+import lombok.Data;
+
+import java.util.Set;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-boot-jwt-sample1
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 1/11/23
+ * Time: 06:03
+ * To change this template use File | Settings | File Templates.
+ */
+@Entity
+@Data
+public class User {
+    @Id
+    private String userName;
+    private String userFirstName;
+    private String userLastName;
+    private String userPassword;
+    @ManyToMany(fetch = FetchType.EAGER, cascade = CascadeType.ALL)
+    @JoinTable(name = "USER_ROLE",
+            joinColumns = {
+                    @JoinColumn(name = "USER_ID")
+            },
+            inverseJoinColumns = {
+                    @JoinColumn(name = "ROLE_ID")
+            }
+    )
+    private Set<Role> role;
+}

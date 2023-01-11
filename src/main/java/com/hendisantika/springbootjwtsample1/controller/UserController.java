4bb79f20cc6dbe920a591bbc15eb43d9bf15534f
hendisantika <hendisantika@yahoo.co.id>
2023-01-12 06:45:51 +0700

    #41 - Add forAdmin in UserController :green_book: :pencil2: :whale:

--- src/main/java/com/hendisantika/springbootjwtsample1/controller/UserController.java
+++ src/main/java/com/hendisantika/springbootjwtsample1/controller/UserController.java
@@ -4,6 +4,8 @@ import com.hendisantika.springbootjwtsample1.entity.User;
 import com.hendisantika.springbootjwtsample1.service.UserService;
 import jakarta.annotation.PostConstruct;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -32,5 +34,11 @@ public class UserController {
     @PostMapping({"/registerNewUser"})
     public User registerNewUser(@RequestBody User user) {
         return userService.registerNewUser(user);
+    }
+
+    @GetMapping({"/forAdmin"})
+    @PreAuthorize("hasRole('Admin')")
+    public String forAdmin() {
+        return "This URL is only accessible to the admin";
     }
 }

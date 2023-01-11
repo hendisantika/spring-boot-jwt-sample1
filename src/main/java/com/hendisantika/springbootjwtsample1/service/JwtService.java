e6ebb8be4f5b6a72bc5a0bd79d6a65b1fbe0328f
hendisantika <hendisantika@yahoo.co.id>
2023-01-12 06:39:44 +0700

    #30 - Add authenticate in JwtService :green_book: :pencil2: :whale:

--- src/main/java/com/hendisantika/springbootjwtsample1/service/JwtService.java
+++ src/main/java/com/hendisantika/springbootjwtsample1/service/JwtService.java
@@ -7,6 +7,9 @@ import com.hendisantika.springbootjwtsample1.repository.UserRepository;
 import com.hendisantika.springbootjwtsample1.util.JwtUtil;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.DisabledException;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
@@ -69,5 +72,15 @@ public class JwtService implements UserDetailsService {
             authorities.add(new SimpleGrantedAuthority("ROLE_" + role.getRoleName()));
         });
         return authorities;
+    }
+
+    private void authenticate(String userName, String userPassword) throws Exception {
+        try {
+            authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(userName, userPassword));
+        } catch (DisabledException e) {
+            throw new Exception("USER_DISABLED", e);
+        } catch (BadCredentialsException e) {
+            throw new Exception("INVALID_CREDENTIALS", e);
+        }
     }
 }

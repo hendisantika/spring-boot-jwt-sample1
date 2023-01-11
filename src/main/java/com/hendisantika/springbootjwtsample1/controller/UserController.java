0ffc141e0566ec6345e475493b34b22996ccc394
hendisantika <hendisantika@yahoo.co.id>
2023-01-12 06:45:02 +0700

    #38 - Add UserController :green_book: :pencil2: :whale:

---
+++ src/main/java/com/hendisantika/springbootjwtsample1/controller/UserController.java
@@ -0,0 +1,22 @@
+package com.hendisantika.springbootjwtsample1.controller;
+
+import com.hendisantika.springbootjwtsample1.service.UserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-boot-jwt-sample1
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 1/12/23
+ * Time: 06:44
+ * To change this template use File | Settings | File Templates.
+ */
+@RestController
+@RequiredArgsConstructor
+public class UserController {
+
+    private final UserService userService;
+}
